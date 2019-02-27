488ba552c42895daa763ecb7cf2b2b26fcd5f3cd
Ola Andersson <ola.bmw.andersson@gmail.com>
2019-02-27 21:53:10 +0100

    First real commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-toolbar app style="background-color: #2C4870">
+      <v-toolbar-title class="headline text-uppercase">
+        <span style="color: white">MTECH</span>
+        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
+      </v-toolbar-title>
+      <!-- <v-spacer></v-spacer> -->
+      <!-- <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>-->
+    </v-toolbar>
+
+    <v-content style="background-color: white">
+
+        <MainComponent/>
+          <!-- TODO How to Send in details to the component -->
+          <ContactComponent/>
+          <ContactComponent/>
+
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import MainComponent from "./components/MainComponent";
+import ContactComponent from "./components/ContactComponent";
+
+export default {
+  name: "App",
+  components: {
+    MainComponent,
+    ContactComponent
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+       <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2.75"
+          ></v-img>
+  
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Namn här</h3>
+              <!-- <div> {{ card_text }} </div> -->
+              <div>Mail: asdf</div>
+              <div>Telefon: asdffsf</div>
+            </div>
+          </v-card-title>
+  
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<style>
+
+</style>
